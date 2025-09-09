5ce33ad1d93af2523d22e960a9852d33dfb05fe1
KiminSon <kimin0331@naver.com>
2025-09-09 10:11:32 +0900

    Initial commit

---
+++ be/src/main/java/com/potg/don/DonApplication.java
@@ -0,0 +1,13 @@
+package com.potg.don;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonApplication.class, args);
+    }
+
+}
