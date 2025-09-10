be6341b835f1cb20d2f95f425dc9b11576adda06
terry <kdjtiger@naver.com>
2025-09-10 13:06:28 +0900

    feat: SSAFY 로그인 버튼 추가

--- fe/src/pages/LoginPage.tsx
+++ fe/src/pages/LoginPage.tsx
@@ -1,18 +1,35 @@
 function LoginPage() {
+  const handleSsafyLogin = () => {
+    // TODO: SSAFY OAuth URL로 리다이렉트
+    // window.location.href = 'SSAFY_OAUTH_URL';
+    console.log('SSAFY 로그인 버튼 클릭됨');
+  };
+
   return (
     <div>
       <h1>로그인</h1>
-      <form>
-        <div>
-          <label>아이디:</label>
-          <input type="text" placeholder="아이디를 입력하세요" />
-        </div>
-        <div>
-          <label>비밀번호:</label>
-          <input type="password" placeholder="비밀번호를 입력하세요" />
-        </div>
-        <button type="submit">로그인</button>
-      </form>
+      
+      {/* SSAFY OAuth 로그인 */}
+      <div style={{ marginBottom: '20px', textAlign: 'center' }}>
+        <button 
+          type="button" 
+          onClick={handleSsafyLogin}
+          style={{ 
+            backgroundColor: '#007bff', 
+            color: 'white',
+            border: 'none',
+            padding: '12px 24px',
+            borderRadius: '4px',
+            fontSize: '16px',
+            cursor: 'pointer',
+            width: '100%',
+            maxWidth: '300px'
+          }}
+        >
+          SSAFY 로그인
+        </button>
+      </div>
+
     </div>
   )
 }
