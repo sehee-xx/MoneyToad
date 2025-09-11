02df6f6a7135aa476fbde0b033ca933b1c46e8b1
sehee <didtpgml0627@gmail.com>
2025-09-12 08:52:47 +0900

    feat: 헤더 메뉴 순서 변경

--- fe/src/components/Header.tsx
+++ fe/src/components/Header.tsx
@@ -44,8 +44,8 @@ export default function Header() {
       <nav className="nav">
         <NavLink to="/"          className={({isActive}) => `nav-item ${isActive ? "active" : ""}`}>마당</NavLink>
         <NavLink to="/leak/:month" className={({isActive}) => `nav-item ${isActive ? "active" : ""}`}>콩쥐의 장독대</NavLink>
-        <NavLink to="/"    className={({isActive}) => `nav-item ${isActive ? "active" : ""}`}>두꺼비의 조언</NavLink>
         <NavLink to="/"  className={({isActive}) => `nav-item ${isActive ? "active" : ""}`}>콩쥐의 씀씀이</NavLink>
+        <NavLink to="/"    className={({isActive}) => `nav-item ${isActive ? "active" : ""}`}>두꺼비의 조언</NavLink>
         <NavLink to="/"  className={({isActive}) => `nav-item ${isActive ? "active" : ""}`}>콩쥐의 곳간</NavLink>
       </nav>
     </header>
