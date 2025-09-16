bcd2c016df64fa4e8ffda1f4cad45a1c735f8b4a
DESKTOP-DLOPF19\SSAFY <rladbstn0425@gmail.com>
2025-09-16 15:16:24 +0900

    refactor: SwaggerConfig 코드 수정

--- be/src/main/java/com/potg/don/config/SwaggerConfig.java
+++ be/src/main/java/com/potg/don/config/SwaggerConfig.java
@@ -1,23 +1,38 @@
 package com.potg.don.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
-@OpenAPIDefinition(
-	servers = {
-		@Server(url = "/", description = "Default Server URL")
-	}
-)
-@SecurityScheme(
-	name = "bearerAuth",
-	type = SecuritySchemeType.HTTP,
-	scheme = "bearer",
-	bearerFormat = "JWT"
-)
 @Configuration
 public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		Info info = new Info()
+			.title("돈꺼비 API")
+			.version("v1.0.0")
+			.description("프로젝트 API에 대한 명세서입니다.");
+
+		return new OpenAPI()
+			.info(info)
+			.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+			.components(new Components().addSecuritySchemes("bearerAuth",
+				new io.swagger.v3.oas.models.security.SecurityScheme()
+					.name("Authorization")
+					.type(SecurityScheme.Type.HTTP)
+					.scheme("bearer")
+					.bearerFormat("JWT")
+			));
+	}
 }
