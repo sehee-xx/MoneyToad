3c67961f6aa90545c4d7cbf034e028c9cace2ca8
terry <kdjtiger@naver.com>
2025-09-18 14:56:52 +0900

    feat: 유저 수정 API 추가

--- fe/src/api/services/users.ts
+++ fe/src/api/services/users.ts
@@ -1,10 +1,25 @@
 import { request, REQUEST_METHOD } from '../client';
 import type { UserInfo } from '../../types/user';
+import type { Gender } from '../../types';
 
 export const getUserInfo = async () => {
   const response = await request<UserInfo>({
     method: REQUEST_METHOD.GET,
     url: `${import.meta.env.VITE_BACK_URL}/api/users`,
+  });
+
+  return response;
+};
+export interface UpdateUserBasicInfo {
+  gender: Gender;
+  age: number;
+}
+
+export const updateUserBasicInfo = async (data: UpdateUserBasicInfo) => {
+  const response = await request<UserInfo>({
+    method: REQUEST_METHOD.PATCH,
+    url: `${import.meta.env.VITE_BACK_URL}/api/users`,
+    data,
   });
 
   return response;
